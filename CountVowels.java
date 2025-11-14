3b1d0f5bb10b49d5bc1439e109b2057cdf53476c
Dibya0912 <pradhandibya0912@gmail.com>
2025-11-14 19:04:43 +0530

    CountVowels

---
+++ CountVowels.java
@@ -0,0 +1,13 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        int count = 0;
+
+        for(char ch : s.toLowerCase().toCharArray()) {
+            if("aeiou".indexOf(ch) != -1)
+                count++;
+        }
+
+        System.out.println("Vowels: " + count);
+    }
+}
