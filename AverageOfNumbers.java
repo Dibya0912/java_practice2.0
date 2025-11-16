dd9d769c7105bfde4fe2c6e6f023afbab273e864
Dibya0912 <pradhandibya0912@gmail.com>
2025-11-16 19:46:27 +0530

    AverageOfNumbers

---
+++ AverageOfNumbers.java
@@ -0,0 +1,12 @@
+public class AverageOfNumbers {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for(int n : arr) sum += n;
+
+        double avg = (double) sum / arr.length;
+
+        System.out.println("Average = " + avg);
+    }
+}
