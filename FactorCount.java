680d69137ed759c49e5cb3162c11df5c94e486a5
Dibya0912 <pradhandibya0912@gmail.com>
2025-11-18 20:20:12 +0530

    FactorCount

---
+++ FactorCount.java
@@ -0,0 +1,13 @@
+public class FactorCount {
+    public static void main(String[] args) {
+        int n = 36;
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                count++;
+        }
+
+        System.out.println("Total factors = " + count);
+    }
+}
